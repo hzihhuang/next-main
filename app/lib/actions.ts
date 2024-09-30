dbd6638e1a13c3fce149a96bb5c8cc7fe46f9260
hzihhuang <hzih.huang@ecoflow.com>
2024-09-30 15:14:58 +0800

    perf: 优化

---
+++ app/lib/actions.ts
@@ -0,0 +1,38 @@
+"use server";
+
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  // 验证表单数据
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+
+  // 向 sql 插入数据
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  // 清除浏览器缓存
+  revalidatePath("/dashboard/invoices");
+  // 重定向到仪表板
+  redirect("/dashboard/invoices");
+}
